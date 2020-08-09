6ac68fd06b8d280ba26623fa1c54ada2ba96733d
ciscoios <petya.bogdan@gmail.com>
2020-08-09 23:42:29 +0300

    Update main.tf

--- modules/gcp/secrets/main.tf
+++ modules/gcp/secrets/main.tf
@@ -18,6 +18,6 @@ resource "google_secret_manager_secret_version" "secret-version-basic" {
    depends_on = [var.local_file]
    secret = google_secret_manager_secret.secret-basic.id
    #secret_data = file(var.tf_ansible_vars_file)
-   secret_data = base64encode(file("${path.root}/ansible/tf_ansible_vars_file.yml.tpl"))
+   secret_data = base64encode(file("${path.root}/ansible/tf_ansible_vars.yml"))
 }
  
