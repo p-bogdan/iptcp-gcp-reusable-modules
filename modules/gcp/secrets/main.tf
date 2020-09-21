c879b7846f32626a75b84d9cb2482c14209cd1a1
Petro Bogdan <you@example.com>
2020-09-21 23:34:03 +0300

    Updating secret_data path

--- modules/gcp/secrets/main.tf
+++ modules/gcp/secrets/main.tf
@@ -17,8 +17,8 @@
 resource "google_secret_manager_secret_version" "secret-version-basic" {
    depends_on = [var.local_file]
    secret = google_secret_manager_secret.secret-basic.id
-   #secret_data = file(var.tf_ansible_vars_file)
-   secret_data = base64decode(file("${path.root}/ansible/${var.tf_ansible_vars_file}"))
+   secret_data = file(base64decode(var.tf_ansible_vars_file))
+  # secret_data = base64decode(file("${path.root}/ansible/${var.tf_ansible_vars_file}"))
 }
 
  
