1bbb1381c8fb160c18f38825b84322d3473a0b9d
Petro Bogdan <you@example.com>
2021-03-14 11:11:04 +0200

    Some minor changes

--- modules/gcp/gke/outputs.tf
+++ modules/gcp/gke/outputs.tf
@@ -3,21 +3,21 @@ output "k8s-cluster-endpoint" {
      sensitive = true
 }
 
-output "cluster_ca_cert" {
-    value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-    sensitive = true
-}
-output "client_certificate" {
-  value     = google_container_cluster.primary.master_auth.0.client_certificate
-  sensitive = true
-}
+# output "cluster_ca_cert" {
+#     value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+#     sensitive = true
+# }
+# output "client_certificate" {
+#   value     = google_container_cluster.primary.master_auth.0.client_certificate
+#   sensitive = true
+# }
 
-output "client_key" {
-  value     = google_container_cluster.primary.master_auth.0.client_key
-  sensitive = true
-}
+# output "client_key" {
+#   value     = google_container_cluster.primary.master_auth.0.client_key
+#   sensitive = true
+# }
 
-output "access_token" {
-    value = data.google_client_config.default.access_token
-    sensitive = true
-}
+# output "access_token" {
+#     value = data.google_client_config.default.access_token
+#     sensitive = true
+# }
