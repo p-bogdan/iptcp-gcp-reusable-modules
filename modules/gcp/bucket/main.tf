5395ec5bc73ceb07f5f6c9a5aea3e804848c230f
Petro Bogdan <you@example.com>
2021-03-25 11:30:03 +0200

    Disabling force bucket destroy

--- modules/gcp/bucket/main.tf
+++ modules/gcp/bucket/main.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "bucket" {
   name = var.bucket_name
   location      = var.location
   uniform_bucket_level_access = true
-  force_destroy = true
+  #force_destroy = true
 }
 
 
