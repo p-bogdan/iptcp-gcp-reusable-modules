75967b985348173a310b4df86d922347a373cd93
p-bogdan <petya.bogdan@gmail.com>
2023-10-04 11:05:51 +0300

    Exposing subnetwork_name output

--- modules/gcp/vpc/output.tf
+++ modules/gcp/vpc/output.tf
@@ -2,6 +2,10 @@ output "network_name" {
   value = google_compute_network.gcp-lab-network.name
 }
 
+output "subnetwork_name" {
+   value = google_compute_network.gcp-lab-subnet.name
+}
+
 output "subnetwork_id" {
 value = google_compute_subnetwork.gcp-lab-subnet.id
 }
