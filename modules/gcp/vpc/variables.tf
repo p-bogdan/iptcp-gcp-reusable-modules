770754cb1d830047e90f96f3198dfe32cd13ebeb
p-bogdan <petya.bogdan@gmail.com>
2023-10-04 10:56:17 +0300

    Exposing subnetwork_name var

--- modules/gcp/vpc/variables.tf
+++ modules/gcp/vpc/variables.tf
@@ -12,6 +12,11 @@ description = "custom network name for gcp lab"
 default = ""
 }
 
+variable "subnetwork_name" {
+description = "custom subnetwork name for gcp lab"
+default = ""
+}
+
 variable "auto_create_subnetworks" {
   type        = bool
   description = "When set to true, the network is created in 'auto subnet mode' and it will create a subnet for each region automatically across the 10.128.0.0/9 address range. When set to false, the network is created in 'custom subnet mode' so the user can explicitly connect subnetwork resources."
