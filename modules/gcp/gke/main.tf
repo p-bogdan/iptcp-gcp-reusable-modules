ad911b4e349efc2e4cc5e833f2aaba5859c1d35a
p-bogdan <petya.bogdan@gmail.com>
2023-10-08 00:06:58 +0300

    Exposing context value

--- modules/gcp/gke/main.tf
+++ modules/gcp/gke/main.tf
@@ -91,7 +91,8 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 resource "local_file" "kubeconfig" {
   #content  = data.template_file.kubeconfig.rendered
   content  = templatefile ("${path.module}/kubeconfig-template.yaml.tfpl", {
-    context                = google_container_cluster.primary.name,
+    #context                = google_container_cluster.primary.name,
+    context                = var.context,
     cluster_ca_certificate = google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
     endpoint               = google_container_cluster.primary.endpoint,
     token                  = data.google_client_config.default.access_token
